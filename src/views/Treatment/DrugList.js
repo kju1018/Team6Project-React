786bf6002c238739139c2ce54a751a0f99966a77
kju1018 <kms951018@naver.com>
2021-06-18 18:07:01 +0900

    height변경

--- src/views/Treatment/DrugList.js
+++ src/views/Treatment/DrugList.js
@@ -1,17 +1,19 @@
+
+
 import PrescriptionHeader from "views/Treatment/components/PrescriptionHeader";
 
 
 function DrugList(props) {
   return (
     <>
-      <PrescriptionHeader headertitle="처방약 목록" buttonname="저장"/>
-      <div className="overflow-auto p-3" style={{height:"calc(100% - 50px"}}>
-        <div className="pt-2 pb-2 mb-2 d-flex" style={{boxShadow:"rgb(0 0 0 / 8%) 0px 0px 5px 2px", borderRadius:"15px", fontSize:"13px"}}>
-          <div className="col-3 pr-0 pl-0 text-center border-left">NIZA15</div>
-          <div className="col-5 pr-0 pl-0 text-center border-left">AXID Cap 150mg</div>
-          <div className="col-3 pr-0 pl-0 text-center border-left">내복약</div>
-          <div className="col-1 pr-0 pl-0 text-center border-left">C</div>
-        </div>
+    <PrescriptionHeader headertitle="처방약 목록" buttonname="검색"/>
+    <div className="overflow-auto p-3" style={{height:"calc(100% - 50px"}}>
+      <div className="pt-2 pb-2 mb-2 d-flex" style={{boxShadow:"rgb(0 0 0 / 8%) 0px 0px 5px 2px", borderRadius:"15px", fontSize:"13px"}}>
+        <div className="col-3 pr-0 pl-0 text-center border-left">NIZA15</div>
+        <div className="col-5 pr-0 pl-0 text-center border-left">AXID Cap 150mg</div>
+        <div className="col-3 pr-0 pl-0 text-center border-left">내복약</div>
+        <div className="col-1 pr-0 pl-0 text-center border-left">C</div>
+      </div>
         
       </div>
     </>
--- src/views/Treatment/PatientWaiting.js
+++ src/views/Treatment/PatientWaiting.js
@@ -1,33 +1,53 @@
+
 import { useState } from "react";
-import { Button, ButtonGroup, ToggleButton } from "react-bootstrap";
-
-
+import { Button, ButtonGroup, Nav, Row, Tab, TabContent, Tabs, ToggleButton } from "react-bootstrap";
 import Item from "views/components/Item";
 import PrescriptionHeader from "./components/PrescriptionHeader";
-
-
+import "views/style/patientWaiting.css";
 
 function PatientWaiting(props) {
 
   const [listtype, setListtype] = useState("wait");
 
-  const handleChange = (event) => {
-    setListtype(event.target.value);
+  const handleChange = (key) => {
+    setListtype(key);
   }
 
   return (
     <>
     <PrescriptionHeader headertitle="환자 리스트" buttonname="환자 검색"/>
-    <div>
-    <ButtonGroup toggle>
-      <ToggleButton type="radio" variant={`${listtype === "wait" ? "secondary" : "light" }`} name="type"  checked={listtype==="wait"} value="wait" onChange={handleChange}><div className="ml-5 mr-5">대기</div></ToggleButton>
-      <ToggleButton type="radio" variant={`${listtype === "complete" ? "secondary" : "light" }`} name="type"  checked={listtype==="complete"} value="complete" onChange={handleChange}><div className="ml-5 mr-5">완료</div></ToggleButton>
-    </ButtonGroup>
-    </div>
-
-    <div className={`overflow-auto p-3`} style={{height:"calc(100% - 80px)"}}>
-      <Item/>
-    </div>
+    <Tab.Container id="left-tabs-example" defaultActiveKey="wait" onSelect={handleChange}>
+      <Nav fill variant="tabs" className="flex-column mb-2">
+        <Row className="ml-0 mr-0">
+          <Nav.Item>
+            <Nav.Link eventKey="wait">대기</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="complete">완료</Nav.Link>
+          </Nav.Item>
+        </Row>
+      </Nav>
+      <Tab.Content className={`overflow-auto`} style={{height:"calc(100% - 90px)"}}>
+        <Tab.Pane eventKey="wait">
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>          
+        </Tab.Pane>
+        <Tab.Pane eventKey="complete">
+          sssssssssdsdfdsf
+        </Tab.Pane>
+      </Tab.Content>
+    </Tab.Container>
     </>
   );
 }
