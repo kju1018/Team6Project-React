fded23650403c2c5c9376487fb58a0315c321ec3
Bongsanggeun <bbsj1058@naver.com>
2021-06-20 14:17:24 +0900

    홈 전체 수정

---
+++ src/views/Home/Photo/index.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Carousel } from "react-bootstrap";
+
+function Photo(props){
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return(
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img src="/doctor.JPG" with="360" height="210"  className="d-block w-100" alt="First slide"/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="/mask.JPG" with="360" height="210" className="d-block w-100" alt="Second slide"/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="/mask2.JPG" with="360" height="210" className="d-block w-100" alt="Third slide"/>
+      </Carousel.Item>
+    </Carousel>
+  )
+}
+
+export default Photo;
---
+++ src/views/Home/Weekly/index.js
@@ -0,0 +1,9 @@
+function Weekly(props){
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default Weekly;
