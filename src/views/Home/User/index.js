2ef5ae5d3da5abb57b3d83466ab4a1dfa1d5fd25
Bongsanggeun <bbsj1058@naver.com>
2021-06-20 14:17:24 +0900

    홈 전체 수정

---
+++ src/views/Home/User/index.js
@@ -0,0 +1,46 @@
+function User(props){
+  return(
+    <>
+    <div className="overflow-auto">
+      <table className="table table-hover">
+        <thead className="thead">
+        <tr class="text-center">
+          <th scope="col">PHOTO</th>
+          <th scope="col">DOCTOR</th>
+          <th scope="col">ROOM</th>
+          <th scope="col">SEX</th>
+        </tr>
+        </thead>
+        <tbody>
+          <tr className="text-center">
+            <th scope="row"><img src="/doctor1.png" width="30" height="30"/></th>
+            <th>김상중</th>
+            <th>1</th>
+            <th>M</th>
+          </tr>
+          <tr className="text-center">
+            <th scope="row"><img src="/doctor2.png" width="30" height="30"/></th>
+            <th>신용권</th>
+            <th>2</th>
+            <th>M</th>
+          </tr>
+          <tr className="text-center">
+            <th scope="row"><img src="/doctor3.png" width="30" height="30"/></th>
+            <th>홍미경</th>
+            <th>3</th>
+            <th>W</th>
+          </tr>
+          <tr className="text-center">
+            <th scope="row"><img src="/doctor3.png" width="30" height="30"/></th>
+            <th>최은지</th>
+            <th>4</th>
+            <th>W</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    </>
+  )
+}
+
+export default User;
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,7 +1,33 @@
+import Notice from "./Notice";
+import User from "./User";
+import Photo from "./Photo";
+import Calendar from "./Calender";
+import Weekly from "./Weekly";
+
 function Home(props) {
   return (
-    <></>
-
+    <div className="container-fluid row vh-100">
+   <div className="col-8">
+      <div className="col card" style={{height:"30vh"}}>
+      <Notice/>
+      </div>
+      <div className="col card"style={{height:"70vh"}}>
+      <Weekly/>
+      </div>
+   </div>
+   <div className="col-4">
+   <div className="col card" style={{height:"40vh"}}>
+      <Calendar/>
+      </div>
+      <div className="col card"style={{height:"30vh"}}>
+      <User/>
+      </div>
+      <div className="col card"style={{height:"30vh"}}>
+      <Photo/>
+      </div>
+   </div>
+    
+    </div>
   );
 }
 
