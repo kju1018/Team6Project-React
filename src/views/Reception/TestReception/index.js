69caf0ae33865ca089de92505bd22bfab74605f6
OhWonjae <dhdnjswo5000@naver.com>
2021-06-23 22:04:32 +0900

    commit!

--- src/views/Reception/TestReception/index.js
+++ src/views/Reception/TestReception/index.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { ToggleButton } from "react-bootstrap";
 import { ButtonGroup } from "react-bootstrap";
 import Item from "views/components/Item";
+import TreatmentButtonHeader from "views/Treatment/components/ButtonHeader";
+import ReceptionHeader from "../components/ReceptionHeader";
 
 function TestReception(props){
     const [listtype, setListtype] = useState("all");
@@ -13,21 +15,19 @@ function TestReception(props){
     }
     const property = ["id","name","room","state","registerday"]
     return(
-        <div className="d-flex flex-column" style={{height:"420px"}}>
-        <div className="row d-flex flex-row justify-content-between">
-                <label style={{marginRight:"10px"}}><i class="bi bi-droplet mr-2"/>검사 접수 환자</label>
-                <div>
+        <div className="pl-3 pr-3 pb-3" style={{height:"435px", backgroundColor:"white"}}>
+            
+            <ReceptionHeader headertitle="검사접수" iclassName="bi bi-droplet " color="#E89677">
                 <button style={{marginRight:"10px"}} className="btn btn-outline-dark btn-sm">검사취소</button>
                 <button style={{marginRight:"10px"}} className="btn btn-outline-dark btn-sm">수정</button>
-                </div>
-        </div>
-        <div>
+            </ReceptionHeader>
+        <div className="mt-1">
             <ButtonGroup toggle>
                 <ToggleButton type="radio" variant={`${listtype === "all" ? "secondary" : "light" }`} name="type"  checked={listtype==="all"} value="all" onChange={handleChange}><div className="ml-5 mr-5">전체</div></ToggleButton>
                 <ToggleButton type="radio" variant={`${listtype === "wait" ? "secondary" : "light" }`} name="type"  checked={listtype==="wait"} value="wait" onChange={handleChange}><div className="ml-5 mr-5">대기</div></ToggleButton>
                 <ToggleButton type="radio" variant={`${listtype === "complete" ? "secondary" : "light" }`} name="type"  checked={listtype==="complete"} value="complete" onChange={handleChange}><div className="ml-5 mr-5">완료</div></ToggleButton>
             </ButtonGroup>
-            </div>
+        </div>
         <div className="rounded-lg justify-content-center">
             <div className="d-flex justify-content-between text-center border " style={{borderRadius:"15px",marginTop:"10px",marginBottom:"10px"}}>
                 <div style={{width:"20%"}}>순번</div>
