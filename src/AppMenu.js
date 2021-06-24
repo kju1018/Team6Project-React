dc526ed83174829051915f7b95b89ea5454da64a
kju1018 <kms951018@naver.com>
2021-06-24 15:24:56 +0900

    수정

--- src/AppMenu.js
+++ src/AppMenu.js
@@ -4,36 +4,36 @@ import { NavLink } from "react-router-dom";
 function AppMenu() {
     return (
     <>
-    <div style={{backgroundColor:"#ECECEC", height:"58vh", marginRight:"-15px"}}></div>
+    <div style={{backgroundColor:"#1B296D", height:"58vh", marginRight:"-15px"}}></div>
     <div style={{marginTop:"3vh"}}>
     <ul className="nav flex-column" >
       <li className="nav-item">
-        <NavLink to="/" exact className="nav-link pt-2 pb-2 mt-3 mb-3" activeStyle={{color:"#000000", backgroundColor:"#9ACAA1"}} style={{color:"black"}}>
+        <NavLink to="/" exact className="nav-link pt-2 pb-2 mt-3 mb-3" activeStyle={{color:"#FFFFFF", backgroundColor:"#2F4273"}} style={{color:"black"}}>
           <div className="row">
             <div className="col-5 text-right"><i className="bi bi-house-door"></i></div>
             <div className="col-7 pl-0">Login</div>
           </div>
         </NavLink>
-        <NavLink to="/home" exact className="nav-link pt-2 pb-2 mt-3 mb-3" activeStyle={{color:"#000000", backgroundColor:"#9ACAA1"}} style={{color:"black"}}>
+        <NavLink to="/home" exact className="nav-link pt-2 pb-2 mt-3 mb-3" activeStyle={{color:"#FFFFFF", backgroundColor:"#2F4273"}} style={{color:"black"}}>
           <div className="row">
             <div className="col-5 text-right"><i className="bi bi-house-door"></i></div>
             <div className="col-7 pl-0">Home</div>
           </div>
         </NavLink>
         {/* Link는 이걸 클릭하면 이동한다 라는 뜻 */}
-        <NavLink to="/reception" className="nav-link pt-2 pb-2 mt-3 mb-3" style={{color:"black"}} activeStyle={{color:"#000000", backgroundColor:"#9ACAA1"}}>
+        <NavLink to="/reception" className="nav-link pt-2 pb-2 mt-3 mb-3" style={{color:"black"}} activeStyle={{color:"#FFFFFF", backgroundColor:"#2F4273"}}>
         <div className="row">
             <div className="col-5 text-right"><i className="bi bi-receipt"></i></div>
             <div className="col-7 pl-0">Reception</div>
           </div>
         </NavLink>
-        <NavLink to="/treatment" className="nav-link pt-2 pb-2 mt-3 mb-3" style={{color:"black"}} activeStyle={{color:"#000000", backgroundColor:"#9ACAA1"}}>
+        <NavLink to="/treatment" className="nav-link pt-2 pb-2 mt-3 mb-3" style={{color:"black"}} activeStyle={{color:"#FFFFFF", backgroundColor:"#2F4273"}}>
           <div className="row">
             <div className="col-5 text-right"><i className="bi bi-calendar-plus"></i></div>
             <div className="col-7 pl-0">Treatment</div>
           </div>
         </NavLink>
-        <NavLink to="/test" className="nav-link pt-2 pb-2 mt-3 mb-3" style={{color:"black"}} activeStyle={{color:"#000000", backgroundColor:"#9ACAA1"}}>
+        <NavLink to="/test" className="nav-link pt-2 pb-2 mt-3 mb-3" style={{color:"black"}} activeStyle={{color:"#FFFFFF", backgroundColor:"#2F4273"}}>
           <div className="row">
             <div className="col-5 text-right"><i className="bi bi-eyedropper"></i></div>
             <div className="col-7 pl-0">Test</div>
--- src/views/Treatment/TreatmentMemo.js
+++ src/views/Treatment/TreatmentMemo.js
@@ -18,7 +18,7 @@ function TreatmentMemo(props) {
 
   return (
   <>
-    <div className="d-flex row pb-1" style={{height:"50px"}}><div className="p-2 ml-3 mr-2 text-center" style={{ backgroundColor:"#6790D8", width:"40px", color:"#FFFFFF"}}><i className="bi bi-clipboard-plus"></i></div>진료 메모</div>
+    <div className="d-flex row pb-1" style={{height:"50px", backgroundColor:"#FFFFFF"}}><div className="p-2 ml-3 mr-2 text-center" style={{ backgroundColor:"#6790D8", width:"40px", color:"#FFFFFF"}}><i className="bi bi-clipboard-plus"></i></div>진료 메모</div>
     <div className="p-3" style={{height:"calc(100% - 50px)"}}>
       <textarea value={props.treatment.memo == (null || "") ? memo : props.treatment.memo} onChange={handlechange} className="h-100 w-100 " style={{boxShadow:"rgb(0 0 0 / 8%) 0px 0px 5px 2px", borderRadius:"15px", height:"50px", outline:0}} >
       </textarea>
