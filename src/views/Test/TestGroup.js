3a1e64bb03dc5424a310bb38e874219807a8bc40
jjung-222 <lyj7877@naver.com>
2021-06-24 15:29:54 +0900

    dd

--- src/views/Test/TestGroup.js
+++ src/views/Test/TestGroup.js
@@ -11,57 +11,112 @@ const intitState = () =>{
   return states;
 }
 function TestGroup(props) {
-  useEffect(()=>{
-    // console.log("hi1")
-    // console.log(props.state)
-  },[props.state])
+  
   const [state, setState] = useState(intitState);
   
-  const changeHandler = (e, stateindex) => { 
-    
+  const changeHandler = (e, index) => { 
+    const modify = state.map((item,i)=>{
+      if(index===i){
+        item.ischeck = e.target.checked
+      }
+      return item;
+    })
+    console.log(modify)
+    setState(modify)
   }
 
-   useEffect(() => {
-      let tmp = intitState;
-      let item;
-        for(var i=0; i<state.length; i++){
-            if(state[i]){
-              //console.log(i)
-              if(state.state==="진행중"){
-                item = {
-                  label: 'primary',
-                  state: '진행중'
-                };
-              }
-              else if(state.state==="대기중"){
-                item =  {
-                  label: 'success',
-                  state: '대기중'
-                };
-              }
-              else{
-                item = {
-                  label: 'danger',
-                  state: '완료'
-                };
-              }
-            tmp[i] = item;         
-        } else {
-          tmp[i] = state[i];
-        }
-      }
-      setState(tmp)
+  const handleStart = (e) => {
+    console.log("클릭")
+    for(var i=0; i<state.length; i++){
+    if(state[i].ischeck === true) {
       
-     
-  },[state.state])
+      }
+    }
+  };
+  const handlePrint = (e) => {
+    // const value = e.target.value;
+    // if (value === "바코드출력") {
+    //   console.log("vv", value)
+    //   let change = 
+    //     {label:"primary", state: "진행중", code:"묶음코드", ischeck:false}
+    //   ;
+    //   setState(change);
+    //   console.log("ss", state)
+    // }
+  };
+  const handleCancel = (e) => {
+    // const value = e.target.value;
+    // if (value === "접수취소") {
+    //   console.log("vv", value)
+    //   let change = 
+    //     {label:"success", state: "대기중", code:"묶음코드", ischeck:false}
+    //   ;
+    //   setState(change);
+    //   console.log("ss", state)
+    // }
+  };
+  const handleFinish = (e) => {
+    // const value = e.target.value;
+    // if (value === "검사완료") {
+    //   console.log("vv", value)
+    //   let change = 
+    //     {label:"danger", state: "완료", code:"묶음코드", ischeck:false}
+    //   ;
+    //   setState(change);
+    //   console.log("ss", state)
+    // }
+  };
+
+  useEffect(() => {
+    let tmp = intitState();
+    let item;
+      for(var i=0; i<state.length; i++){
+          if(state[i].ischeck === true){
+            //console.log(i)
+            if(state==="진행중"){
+              item = {
+                label: 'primary',
+                state: '진행중'
+              };
+            }
+            else if(state==="대기중"){
+              item =  {
+                label: 'success',
+                state: '대기중'
+              };
+            }
+            else{
+              item = {
+                label: 'danger',
+                state: '완료'
+              };
+            }
+          tmp[i] = item;         
+      } else {
+        tmp[i] = state[i];
+      }
+    }
+    //setState(tmp)
+},[state])
+
+
+
   return (
+    <>
+    <div className="mt-4 mb-2 text-right">
+      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handleStart } value="검사시작">검사시작</button>
+      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handlePrint } value="바코드출력">바코드출력</button>
+      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handleCancel } value="접수취소">접수취소</button>
+      <button type="button" className="btn btn-dark btn-sm mr-1">엑셀저장</button>
+      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handleFinish } value="검사완료">검사완료</button>
+    </div>
     <div className="overflow-auto" style={{height:"750px", backgroundColor:"#ffffff"}}>
     <Accordion defaultActiveKey="0">
     {state.map((item,index)=>{return(
       <Card>
       <Card.Header>
         <Accordion.Toggle as={Button} variant="link" eventKey={index.toString()}>
-          <input type="checkbox" onChange={e => {changeHandler(e, index)}} value={state[index].ischeck}/>{state[index].code} <Badge variant={state[index].label}>{state[index].code}</Badge>
+          <input type="checkbox" onChange={e => {changeHandler(e, index)}} value={item.ischeck}/>{item.code} <Badge variant={item.label}>{item.state}</Badge>
         </Accordion.Toggle>
       </Card.Header>
       <Accordion.Collapse eventKey={index.toString()}>
@@ -73,6 +128,7 @@ function TestGroup(props) {
     })}
    </Accordion>
     </div>
+  </>
   );
 }
 export default TestGroup;
--- src/views/Test/index.js
+++ src/views/Test/index.js
@@ -8,55 +8,10 @@ import TestWaitingHeader from "./TestWaitingHeader";
 import TestWaitingList from "./TestWaitingList";
 
 function TestPage(props) { 
-  const object = {label:"success", state: "대기중"};
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const [state, setState] = useState(object);
-  console.log("렌더링")
-  useEffect(()=>{
-    console.log("hi2")
-    console.log(state)
-  },[state])
-  // const handleStart = (e) => {
-  //   const { value } = e.target;
-  //   if (value === "검사시작") {
-  //     console.log("vv", value)
-  //     let change = 
-  //       {label:"primary", state: "진행중"}
-  //     ;
-  //     setState(change);
-  //   }
-  // };
-  const handlePrint = (e) => {
-    const { value } = e.target;
-    if (value === "바코드출력") {
-      let change = 
-        {label:"primary", state: "진행중"}
-      ;
-      setState(change);
-    }
-  };
-  const handleCancel = (e) => {
-    const { value } = e.target;
-    if (value === "접수취소") {
-      let change = 
-        {label:"success", state: "대기중"}
-      ;
-      setState(change);
-    }
-  };
-  const handleFinish = (e) => {
-    const { value } = e.target;
-    if (value === "검사완료") {
-      let change = 
-        {label:"danger", state: "완료"}
-      ;
-      setState(change);
-    }
-  };
 
   return (
     <div className="100vh" style={{minWidth:"1000px"}}>
@@ -72,15 +27,7 @@ function TestPage(props) {
         <div className="col-4 mt-5" style={{borderRight:"1px solid #dadada"}}>
           <div>
           <div className="row pl-2" style={{backgroundColor: "#ffffff", width:"85%"}}><div className="pr-3 pl-3 pt-2 pb-2" style={{ backgroundColor:"#F2E18D"}}><i class="bi bi-droplet" style={{ fontSize:"22px"}}></i></div><div className="ml-4 pt-2">검사 처방 목록</div></div>
-            <div className="mt-4 mb-2 text-right">
-              <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handlePrint } value="바코드출력">바코드출력</button>
-              <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handleCancel } value="접수취소">접수취소</button>
-              <button type="button" className="btn btn-dark btn-sm mr-1">엑셀저장</button>
-              <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handleFinish } value="검사완료">검사완료</button>
-            </div>
-            <div>
-              <TestGroup state={state}/>
-            </div>
+            <TestGroup/>
           </div>
         </div>
         <div className="col-5 mt-5">
