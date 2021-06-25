670c8857d5dfe9e9703988796780b076f2318476
jjung-222 <lyj7877@naver.com>
2021-06-25 15:47:27 +0900

    cc

--- src/views/Test/TestWaitingList.js
+++ src/views/Test/TestWaitingList.js
@@ -11,11 +11,11 @@ function TestWaitingList(props) {
     <>
       <div className="pt-2 pb-2 mb-2 d-flex align-items-center" style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
         <div className="col-2 p-0 pt-1 pb-1 text-center">1</div>
-              <div className="col-3 p-0 text-center">13232</div>
-              <div className="col-2 p-0 text-center">F/29</div>
-              <div className="col-2 p-0 text-center">이연정</div>
-              <div className="col-3 p-0 text-center"><Badge className="mr-1" variant="primary">진행중</Badge><Badge variant="danger">미입력</Badge></div>
-        </div>
+        <div className="col-3 p-0 text-center">13232</div>
+        <div className="col-2 p-0 text-center">F/29</div>
+        <div className="col-2 p-0 text-center">이연정</div>
+        <div className="col-3 p-0 text-center"><Badge className="mr-1" variant="primary">진행중</Badge><Badge variant="danger">미입력</Badge></div>
+      </div>
     </>
   );
 }
---
+++ src/views/Test/data/patient.js
@@ -0,0 +1,16 @@
+let lastBno = 15;
+
+let data = [];
+
+for(var i=1; i<=lastBno; i++) {
+  data.push({
+    patientid:i, 
+    patientname:"환자"+i, 
+    sex: "F", 
+    ssn1:"951124", 
+  });
+}
+
+export function getPatients() { //list를 리턴하는 함수
+ 
+};
