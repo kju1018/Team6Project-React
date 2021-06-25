7d0f684d099241722ed2f13a11472797c7a244db
Bongsanggeun <bbsj1058@naver.com>
2021-06-25 15:23:13 +0900

    hi

--- src/views/Home/Calendar/index.js
+++ src/views/Home/Calendar/index.js
@@ -5,14 +5,13 @@ import "./datepickerHome.css";
 function Calendar(props) {
   const [startDate, setStartDate] = useState(new Date());
   return (
-    <div className="col">
     <DatePicker
       selected={startDate}
       onChange={(date) => setStartDate(date)}
+      monthsShown={2}
       inline
       
     />
-    </div>
   );
 };
 
