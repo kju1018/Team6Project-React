04b7c32b1825f80d62e14997e2a1f1dde3abee1f
OhWonjae <dhdnjswo5000@gmail.com>
2021-06-25 10:16:58 +0900

    wonjae ocmmit!

--- src/views/components/Menu.js
+++ src/views/components/Menu.js
@@ -11,14 +11,11 @@ function Menus(props){
     if(history.location.pathname==="/reception" || history.location.pathname==="/treatment" || history.location.pathname==="/test"){
       isMenuPath  = true; console.log("path1"+isMenuPath)
     }
-    console.log("path1menua")
   },[useHistory()])
   
   return (
     
  <Menu right noOverlay  width={"30%"}>
-   {console.log("path2"+isMenuPath)}
-   
      <TreatmentReception></TreatmentReception>
      <TestReception></TestReception>
   </Menu>
