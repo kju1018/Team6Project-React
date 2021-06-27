58580334d7de09e1410ddf1dc65871c126daa96c
Bongsanggeun <bbsj1058@naver.com>
2021-06-27 13:45:06 +0900

    hi

--- src/views/Home/Weekly/WeeklyForm/index.js
+++ src/views/Home/Weekly/WeeklyForm/index.js
@@ -13,14 +13,6 @@ function WeeklyForm(props){
               <Form>
                 <h4 className="text-center">WEEKLY <img src="/weekly.png"width="25"height="25"/></h4>
                 <hr></hr>
-                <Form.Group className="mb-3, text-center" controlId="exampleForm.ControlInput1">
-                  <Form.Label><strong>담당 의사</strong></Form.Label>
-                  <Form.Control type="text"/>
-                </Form.Group>
-                <Form.Group className="mb-3, text-center" controlId="exampleForm.ControlInput1">
-                  <Form.Label><strong>예약 시간</strong></Form.Label>
-                  <Form.Control type="text"/>
-                </Form.Group>
                 <Form.Group className="mb-3, text-center" controlId="exampleForm.ControlTextarea1">
                   <Form.Label><strong>내용</strong></Form.Label>
                   <Form.Control as="textarea" rows={3} />
