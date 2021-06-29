ec1df57dd74a7750312baf25cab15169544a8428
jjung-222 <lyj7877@naver.com>
2021-06-29 18:59:23 +0900

    d

--- src/views/Test/TestGroup.js
+++ src/views/Test/TestGroup.js
@@ -9,27 +9,58 @@ function TestGroup(props) {
   const [state, setState] = useState(testList()); //묶음 코드 객체
   const [open, setOpen] = useState(false); //모달 열림/닫힘 상태
   const [testdata, setTestData] = useState(testList());
+  const [testlist, setTestList] = useState(testData())
+  const [groupList, setGroupList] = useState({});
+  const [grouptitle, setGroupTitle] = useState([]);
+  
+  let excel = [];
   var pass = props.clickdate
   var id = props.passdata
-  
-  useEffect(()=>{ 
-    for(var i=0; i<state.length; i++){
-      if(id.testreceptionid === state[i].testreceptionid){ 
-        for(var j=0; j<testdata.length; j++){
-          if(state[i].testdataid === testdata[j].testdataid){
-            console.log(state[i])
-            
+  const patienttest = testList();
 
+  useEffect(()=>{ 
+    const group = [];
+    const temp = [];
+    for(var i=0; i<state.length; i++){
+      if(id.testreceptionid === patienttest[i].testreceptionid){ 
+        for(var j=0; j<testdata.length; j++){
+          if(state[i].testdataid === testlist[j].testdataid){
+            const put = {...testlist[j], state:patienttest[i].state}
+            temp.push(put)        
           }
         } 
       }    
     }
-    console.log(id.testreceptionid)
+    for(var i=0; i<temp.length; i++){
+      group.push(temp[i].groupcode)
+    }
+    const set = new Set(group)
+    setGroupTitle([...set]); //묶음 코드 중복 제거
+    
+    const title = [...set];
+
+    let obj = {};
+    for(var i=0; i<title.length; i++){
+        for(var j=0; j<temp.length; j++){
+          if(title[i] === temp[j].groupcode) {
+            if(obj[title[i]]){
+              obj[title[i]].push(temp[j]);
+            } else {
+              obj[title[i]]=[];
+              obj[title[i]].push(temp[j]);
+            }
+          }
+       }
+    }
+    setGroupList(obj);
+
+    console.log(temp)
+
   }, [id.testreceptionid])
 
 
   const handleExcel =() => { //엑셀 버튼 클릭 시, 동작하는 함수
-    const ws = xlsx.utils.json_to_sheet(test); //안에 배열의 객체 넣으면 그대로 출력
+    const ws = xlsx.utils.json_to_sheet(testdata); //안에 배열의 객체 넣으면 그대로 출력
     const wb = xlsx.utils.book_new();
     xlsx.utils.book_append_sheet(wb, ws, "Sheet1");
     xlsx.writeFile(wb, "Test.xlsx");
@@ -153,7 +184,6 @@ function TestGroup(props) {
   const Save = () => {
     alert("저장")
   }
-
   return (
     <>
     <div className="mt-4 mb-2 text-right">
@@ -167,12 +197,13 @@ function TestGroup(props) {
 
     <div className="overflow-auto" style={{height:"700px"}}>
       <Accordion defaultActiveKey="0">
-        {state.map((item,index)=>{return(
+      {grouptitle.map((item,index)=>{
+        return(
           <Card>
           <Card.Header className="row" style={{backgroundColor:"#D5D5D5", height:"60px", alignItems:"center"}}>
             <Accordion.Toggle as={Card.Header} eventKey={index.toString()}>
               {/* checked: 체크박스 체크 유무 */}
-              <div><input className="mr-2" type="checkbox" onChange={e => {changeHandler(e, index)}} checked={item.ischeck}/>{item.groupcode}<Badge className="ml-3" variant={item.label}>{item.state}</Badge></div>
+              <div><input className="mr-2" type="checkbox" onChange={e => {changeHandler(e, index)}} checked={item.ischeck}/>{item}<Badge className="ml-3" variant={item.label}>{item.state}</Badge></div>
             </Accordion.Toggle>
             <div>검사자: {item.tester}</div>
           </Card.Header>
@@ -183,28 +214,21 @@ function TestGroup(props) {
                 <div className="col-2 p-0 text-center">처방코드</div>
                 <div className="col-2 p-0 text-center">검사명</div>
                 <div className="col-2 p-0 text-center">용기</div>
-                <div className="col-2 p-0 text-center">진료의</div>
+                <div className="col-2 p-0 text-center">상태</div>
                 <div className="col-2 p-0 text-center">결과값</div>
               </div>
-
-              <div className="pt-2 pb-2 mb-2 d-flex align-items-center" style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
-                <div className="col-2 p-0 pt-1 pb-1 text-center">1</div>
-                <div className="col-2 p-0 text-center">E7401</div>
-                <div className="col-2 p-0 text-center">순환기능검사</div>
-                <div className="col-2 p-0 text-center">EDTA</div>
-                <div className="col-2 p-0 text-center">신용권</div>
-                <div className="col-2 p-0 text-center"> <input type="text" style={{width:"100%"}} ></input></div>
-               
-              </div>
-
-              <div className="pt-2 pb-2 mb-2 d-flex align-items-center" style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
-                <div className="col-2 p-0 pt-1 pb-1 text-center">2</div>
-                <div className="col-2 p-0 text-center">B1020</div>
-                <div className="col-2 p-0 text-center">헤마토 크리트</div>
-                <div className="col-2 p-0 text-center">EDTA</div>
-                <div className="col-2 p-0 text-center">신용권</div>
-                <div className="col-2 p-0 text-center"> <input type="text" style={{width:"100%"}} ></input></div>
-              </div>
+              {groupList[item].map((test, index) => {
+                return (
+                  <div className="pt-2 pb-2 mb-2 d-flex align-items-center" style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
+                    <div className="col-2 p-0 pt-1 pb-1 text-center">{index}</div>
+                    <div className="col-2 p-0 text-center">{test.testdataid}</div>
+                    <div className="col-2 p-0 text-center">{test.testdataname}</div>
+                    <div className="col-2 p-0 text-center">EDTA</div>
+                    <div className="col-2 p-0 text-center">{test.state}</div>
+                    <div className="col-2 p-0 text-center"> <input type="text" style={{width:"100%"}} ></input></div>
+                  </div>
+                )
+              })}
               
               <div className="mt-3 text-right"><button className="btn btn-info btn-sm mr-2" disabled={item.saveBtn} onClick={Save}>저장</button></div>
             </Card.Body>
