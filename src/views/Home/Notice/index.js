5d397aff48c237c6c944c07927e05dff8990c456
Bongsanggeun <bbsj1058@naver.com>
2021-06-30 16:17:36 +0900

    hi

--- src/views/Home/Notice/index.js
+++ src/views/Home/Notice/index.js
@@ -5,19 +5,21 @@ import Item from "views/components/Item";
 import "./scrollbar1.css"
 function Notice(props){
   const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
   const [selectedNotice, setSelectedNotice] = useState({});
   //공지사항을 가져올 list 
 
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const handleClose1 = () => setShow(false);
-  const handleShow1 = () => setShow(true);
+  
+  const handleClose1 = () => setShow1(false);
+  const handleShow1 = () => setShow1(true);
 
-  const selectNotice = (notice) => {
-    setSelectedNotice(notice);
-    handleShow();
-  }
+  // const selectNotice = (notice) => {
+  //   setSelectedNotice(notice);
+  //   handleShow();
+  // }
 
 return(
   <>
@@ -62,54 +64,54 @@ return(
       <tr>
         <th scope="row">1</th>
         <th>신용권</th>
-        <th>안녕<button  variant="primary" type="button" class="btn btn-outline-success btn-sm" onClick={{handleShow1}} style={{float: "right"}}>
+        <th>병원내 코로나 방역수칙안내<button  variant="primary" type="button" class="btn btn-outline-success btn-sm" onClick={handleShow1} style={{float: "right"}}>
             자세히
           </button>
-          <Modal show={show} onHide={handleClose1}>
-            <Modal.Header closeButton>
-              <Modal.Title>dd</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>ds</Modal.Body>
-            <Modal.Footer>
-              <Button variant="primary" onClick={handleClose1}>
-                CLOSE
-              </Button>
-            </Modal.Footer>
-          </Modal>
+          <Modal show={show1} onHide={handleClose1}>
+          <Modal.Header closeButton>
+            <Modal.Title>병원내 코로나 방역수칙안내</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>최근 코로나 확산이 지속되고 있습니다. 개인위생을 철저히 하여 우리모두 코로나를 예방합시다.</Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleClose1}>
+              CLOSE
+            </Button>
+          </Modal.Footer>
+        </Modal>
         </th>
         <th>2021-06-16</th>
        </tr>
        <tr>
-        <th scope="row">1</th>
-        <th>신용권</th>
-        <th>안녕<button  variant="primary" type="button" class="btn btn-outline-success btn-sm" onClick={{handleShow}} style={{float: "right"}}>
-            자세히
-          </button>
-        </th>
-        <th>2021-06-16</th>
-       </tr>
-       <tr>
-        <th scope="row">1</th>
-        <th>신용권</th>
-        <th>안녕<button  variant="primary" type="button" class="btn btn-outline-success btn-sm" onClick={{handleShow}} style={{float: "right"}}>
+        <th scope="row">2</th>
+        <th>김상중</th>
+        <th>병원내 휴무일 안내<button  variant="primary" type="button" class="btn btn-outline-success btn-sm" onClick={{handleShow}} style={{float: "right"}}>
             자세히
           </button>
         </th>
         <th>2021-06-16</th>
        </tr>
        <tr>
-        <th scope="row">1</th>
+        <th scope="row">3</th>
         <th>신용권</th>
-        <th>안녕<button  variant="primary" type="button" class="btn btn-outline-success btn-sm" onClick={{handleShow}} style={{float: "right"}}>
+        <th>병원 근무일수 안내<button  variant="primary" type="button" class="btn btn-outline-success btn-sm" onClick={{handleShow}} style={{float: "right"}}>
             자세히
           </button>
         </th>
         <th>2021-06-16</th>
        </tr>
        <tr>
-        <th scope="row">1</th>
-        <th>신용권</th>
-        <th>안녕<button  variant="primary" type="button" class="btn btn-outline-success btn-sm" onClick={{handleShow}} style={{float: "right"}}>
+        <th scope="row">4</th>
+        <th>최은지</th>
+        <th>안내사항<button  variant="primary" type="button" class="btn btn-outline-success btn-sm" onClick={{handleShow}} style={{float: "right"}}>
+            자세히
+          </button>
+        </th>
+        <th>2021-06-16</th>
+       </tr>
+       <tr>
+        <th scope="row">5</th>
+        <th>홍미경</th>
+        <th>병원 공지<button  variant="primary" type="button" class="btn btn-outline-success btn-sm" onClick={{handleShow}} style={{float: "right"}}>
             자세히
           </button>
         </th>
