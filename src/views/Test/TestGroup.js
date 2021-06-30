a882afe0082feb5b8f99210f1fb4dd83240a9e4b
jjung-222 <lyj7877@naver.com>
2021-06-30 11:07:55 +0900

    hi

--- src/views/Test/TestGroup.js
+++ src/views/Test/TestGroup.js
@@ -3,183 +3,134 @@ import { Badge, Button, Modal, Accordion, Card  } from "react-bootstrap";
 import xlsx from 'xlsx';
 import React from 'react';
 import Print from "./Print";
-import { testList, testData } from "./data/patient"
+import { AlltestList, AlltestData, startTests } from "./data/patient"
 
 function TestGroup(props) {
-  const [state, setState] = useState(testList()); //묶음 코드 객체
+  const [state, setState] = useState(); //묶음 코드 객체
   const [open, setOpen] = useState(false); //모달 열림/닫힘 상태
-  const [testdata, setTestData] = useState(testList());
-  const [testlist, setTestList] = useState(testData())
-  const [groupList, setGroupList] = useState({});
-  const [grouptitle, setGroupTitle] = useState([]);
-  
-  let excel = [];
-  var pass = props.clickdate
-  var id = props.passdata
-  const patienttest = testList();
+  const [testlist, setTestData] = useState(AlltestList());
+  const [testdata, setTestList] = useState(AlltestData())
+  const [groupList, setGroupList] = useState({}); //제일 바깥
+
+  const patienttest = AlltestList();
 
   useEffect(()=>{ 
+  
     const group = [];
     const temp = [];
-    for(var i=0; i<state.length; i++){
-      if(id.testreceptionid === patienttest[i].testreceptionid){ 
-        for(var j=0; j<testdata.length; j++){
-          if(state[i].testdataid === testlist[j].testdataid){
-            const put = {...testlist[j], state:patienttest[i].state}
+    for(var i=0; i<testlist.length; i++){
+      if(props.clickdate.testreceptionid === patienttest[i].testreceptionid){ 
+        for(var j=0; j<testlist.length; j++){
+          if(testlist[i].testdataid === testdata[j].testdataid){
+            const put = {...testdata[j], state:patienttest[i].state}
             temp.push(put)        
           }
         } 
       }    
-    }
+    } //receptionid에 맞는 test 가져오기
+
+
     for(var i=0; i<temp.length; i++){
-      group.push(temp[i].groupcode)
+      group.push(temp[i].groupcode)    
     }
     const set = new Set(group)
-    setGroupTitle([...set]); //묶음 코드 중복 제거
-    
-    const title = [...set];
+    const title = [...set]; //묶음 코드 중복 제거
 
-    let obj = {};
+
+    let obj = {};//나중에 groupList가 될 친구 데이터 가공후 리스트에 추가
     for(var i=0; i<title.length; i++){
         for(var j=0; j<temp.length; j++){
           if(title[i] === temp[j].groupcode) {
-            if(obj[title[i]]){
-              obj[title[i]].push(temp[j]);
+            if(obj[title[i]]){ //그룹코드이름으로 된 속성이 있을 때
+              obj[title[i]].tests.push(temp[j]);
             } else {
-              obj[title[i]]=[];
-              obj[title[i]].push(temp[j]);
+              obj[title[i]]={};
+              obj[title[i]].groupcode=temp[j].groupcode;
+              obj[title[i]].groupname=temp[j].groupname;
+              obj[title[i]].state=temp[j].state;
+              obj[title[i]].ischeck=false;
+              if(temp[j].state === "검사완료") {
+                obj[title[i]].saveBtn=false;
+                obj[title[i]].label = "danger";
+              } else if (temp[j].state === "진행중") {
+                obj[title[i]].saveBtn=true;
+                obj[title[i]].label = "primary";
+              } else {
+                obj[title[i]].saveBtn=true;
+                obj[title[i]].label = "success";
+              };
+              obj[title[i]].tests=[];
+              obj[title[i]].tests.push(temp[j]);
             }
           }
        }
     }
     setGroupList(obj);
 
-    console.log(temp)
+  }, [props.clickdate.testreceptionid])
 
-  }, [id.testreceptionid])
-
-
-  const handleExcel =() => { //엑셀 버튼 클릭 시, 동작하는 함수
-    const ws = xlsx.utils.json_to_sheet(testdata); //안에 배열의 객체 넣으면 그대로 출력
-    const wb = xlsx.utils.book_new();
-    xlsx.utils.book_append_sheet(wb, ws, "Sheet1");
-    xlsx.writeFile(wb, "Test.xlsx");
-  }
-
-  const handlePrint = () => { //바코드 생성 버튼
-    let checkItems = [];
-    let flag = 0;
-    for(var i=0; i<state.length; i++){
-      if(state[i].ischeck === true){ //체크박스 선택 시, 
-        if(state[i].state === "진행중") {
-          alert("이미 검사가 진행중입니다.")
-          const item = {...state[i], ischeck:false}
-          checkItems.push(item);
-          flag = 1;
-        } else if(state[i].state === "검사완료"){
-          alert("이미 검사가 완료되었습니다.");
-          const item = {...state[i], ischeck:false}
-          checkItems.push(item);
-          flag = 1;
-        } else {//대기중
-          const item = {...state[i], state:"진행중", tester:"이연정", label:"primary", ischeck:false, saveBtn:true}
-          checkItems.push(item);
-        }
-      } else {
-        checkItems.push(state[i]);
-      }
-    }
-    setState(checkItems); //체크하고 버튼 누르면 자동으로 체크 해제
-    if(flag === 0) {
-      setOpen(true); 
-    }
-  }
 
   const handleExit = () => setOpen(false); //바코드 모달 닫힘
 
 
-  const changeHandler = (e, index) => { //체크버튼 선택 시, ischeck 변경해줌 
-    const modify = state.map((item,i)=>{
-      if(index===i){
-        item.ischeck = e.target.checked
+  const changeHandler = (e, groupcode) => { //체크버튼 선택 시, ischeck 변경해줌 
+    const modify = Object.values(groupList).map((group)=>{
+      if(group.groupcode===groupcode){
+        group.ischeck = e.target.checked
       }
-      return item;
+      return group;
     })
-    console.log(modify)
-    setState(modify)
+    console.log(modify);
+    setGroupList(modify)
   }
 
-  const handleStart = (e) => { //검사 시작
-    let checkItem = [];
+  const handleStart = (groupList) => {
+    let checkedList = [];
+    let flag = 0;
     
-    for(var i=0; i<state.length; i++) {
-      if(state[i].ischeck === true){
-        if(state[i].state === "진행중"){
-          alert("이미 검사가 진행중입니다.")
-          const item = {...state[i], ischeck:false}
-          checkItem.push(item)
-        } else if (state[i].state === "검사완료") {
-          alert("이미 검사가 완료되었습니다.")
-          const item = {...state[i], ischeck:false}
-          checkItem.push(item)
+    const newGroupList = Object.values(groupList).map ((group) => {
+      if(group.ischeck === true){
+        group.ischeck = false;
+        if(group.state === "대기중"){
+          checkedList.push(group);
         } else {
-          const item = {...state[i], state:"진행중", tester:"이연정", label:"primary", ischeck:false, saveBtn:true}
-          checkItem.push(item)
+          flag = 1;
         }
-      } else {
-        checkItem.push(state[i]);
       }
+      return group;
+    });
+    if(flag === 0){
+      startTests(checkedList);
     }
-    setState(checkItem)
-  };
-  
-  const handleCancel = (e) => { //검사 취소
-    let checkItem = [];
     
-    for(var i=0; i<state.length; i++) {
-      if(state[i].ischeck === true){
-        if(state[i].state === "대기중"){
-          alert("검사가 대기중입니다.")
-          const item = {...state[i], ischeck:false}
-          checkItem.push(item)
-        } else if (state[i].state === "검사완료") {
-          alert("이미 검사가 완료되었습니다.")
-          const item = {...state[i], ischeck:false}
-          checkItem.push(item)
-        } else {
-          const item = {...state[i], state:"대기중", tester:"이연정", label:"success", ischeck:false, saveBtn:true}
-          checkItem.push(item)
-        }
-      } else {
-        checkItem.push(state[i]);
-      }
-    }
-    setState(checkItem)
-  };
+    setGroupList(newGroupList);
+  }
 
-  const handleFinish = (e) => { //검사 완료
-    let checkItem = [];
-    
-    for(var i=0; i<state.length; i++) {
-      if(state[i].ischeck === true){
-        if(state[i].state === "대기중"){
-          alert("먼저 검사를 진행해주세요.")
-          const item = {...state[i], ischeck:false}
-          checkItem.push(item)
-        } else if (state[i].state === "검사완료") {
-          alert("이미 검사가 완료되었습니다.")
-          const item = {...state[i], ischeck:false}
-          checkItem.push(item)
-        } else {
-          const item = {...state[i], state:"검사완료", tester:"이연정", label:"danger", ischeck:false, saveBtn:true}
-          checkItem.push(item)
-        }
-      } else {
-        checkItem.push(state[i]);
+  const handleExcel =() => { //엑셀 버튼 클릭 시, 동작하는 함수
+    const ws = xlsx.utils.json_to_sheet(testlist); //안에 배열의 객체 넣으면 그대로 출력
+    const wb = xlsx.utils.book_new();
+    xlsx.utils.book_append_sheet(wb, ws, "Sheet1");
+    xlsx.writeFile(wb, "처방검사.xlsx");
+  }
+
+  const handlePrint = () => { 
+    const newGroupList = Object.values(groupList).map ((group) => {
+      if(group.ischeck === true){
+        setOpen(true);
+        group.ischeck = false;
       }
-    }
-    setState(checkItem)
-  };
+      return group;
+    });
+    setGroupList(newGroupList);
+  }
+
+  const handleCancel =() => { 
+  
+  }
+
+  const handleFinish =() => { 
+  
+  }
 
   const Save = () => {
     alert("저장")
@@ -187,25 +138,26 @@ function TestGroup(props) {
   return (
     <>
     <div className="mt-4 mb-2 text-right">
-      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handleStart } value="검사시작">검사시작</button>
-      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handlePrint } value="바코드출력">바코드출력</button>
-      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handleCancel } value="접수취소">접수취소</button>
-      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handleExcel }>엑셀저장</button>
-      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handleFinish } value="검사완료">검사완료</button>
-    </div>
+      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ () => { handleStart(groupList) }} value="검사시작">검사시작</button>
+      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ () => {handlePrint(groupList) }} value="바코드출력">바코드출력</button>
+      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ () => {handleCancel(groupList) }} value="접수취소">접수취소</button>
+      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ handleExcel}>엑셀저장</button>
+      <button type="button" className="btn btn-dark btn-sm mr-1" onClick={ () => {handleFinish(groupList) }} value="검사완료">검사완료</button>
+    </div> 
 
 
     <div className="overflow-auto" style={{height:"700px"}}>
       <Accordion defaultActiveKey="0">
-      {grouptitle.map((item,index)=>{
-        return(
+       {groupList !=={} &&
+       Object.values(groupList).map((group, index)=> {
+         return (
           <Card>
           <Card.Header className="row" style={{backgroundColor:"#D5D5D5", height:"60px", alignItems:"center"}}>
             <Accordion.Toggle as={Card.Header} eventKey={index.toString()}>
               {/* checked: 체크박스 체크 유무 */}
-              <div><input className="mr-2" type="checkbox" onChange={e => {changeHandler(e, index)}} checked={item.ischeck}/>{item}<Badge className="ml-3" variant={item.label}>{item.state}</Badge></div>
+              <div><input className="mr-2" type="checkbox"  onChange={e => {changeHandler(e, group.groupcode)}} checked={group.ischeck}/>{group.groupcode}<Badge className="ml-3" variant={group.label}>{group.state}</Badge></div>
             </Accordion.Toggle>
-            <div>검사자: {item.tester}</div>
+            <div></div>
           </Card.Header>
           <Accordion.Collapse eventKey={index.toString()}>
             <Card.Body>
@@ -217,7 +169,7 @@ function TestGroup(props) {
                 <div className="col-2 p-0 text-center">상태</div>
                 <div className="col-2 p-0 text-center">결과값</div>
               </div>
-              {groupList[item].map((test, index) => {
+              {group.tests.map((test, index) => {
                 return (
                   <div className="pt-2 pb-2 mb-2 d-flex align-items-center" style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
                     <div className="col-2 p-0 pt-1 pb-1 text-center">{index}</div>
@@ -230,12 +182,12 @@ function TestGroup(props) {
                 )
               })}
               
-              <div className="mt-3 text-right"><button className="btn btn-info btn-sm mr-2" disabled={item.saveBtn} onClick={Save}>저장</button></div>
+              <div className="mt-3 text-right"><button className="btn btn-info btn-sm mr-2" disabled={group.saveBtn} onClick={Save}>저장</button></div>
             </Card.Body>
           </Accordion.Collapse>
         </Card>
-        )
-        })} 
+         )
+       })}
       </Accordion>
     </div>
 
--- src/views/Test/index.js
+++ src/views/Test/index.js
@@ -3,42 +3,41 @@ import PeriodSearch from "./PeriodSearch";
 import TestGroup from "./TestGroup";
 import TestResult from "./TestResult";
 import { Nav, Row, Tab, Badge } from "react-bootstrap";
-import {getAllPatient, waitingPatient, progressPatient, completePatient, testDate, testList } from "./data/patient"
+import {getAllPatient, waitingPatient, progressPatient, completePatient, testReceptions } from "./data/patient"
 
 function TestPage(props) {  
-  let list=[];
-  const [show, setShow] = useState(false);
-  const [testreceptions, setTestReception] = useState(testDate())
-  const [clickdate, setClickDate] = useState(list);
-  const [profile, setProfile] = useState({});
-  const [patient, setPatient] = useState(getAllPatient())
-  const [waiting, setWaiting] = useState(waitingPatient())
-  const [progress, setProgress] = useState(progressPatient())
-  const [complete, setComplete] = useState(completePatient())
-  const [groupshow, setGroupShow] = useState(false)
-  const [passdata, setPassData] = useState({})
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [testreceptions, setTestReception] = useState(testReceptions()) //전체 환자 접수 기록
 
+  const [patients, setPatient] = useState(getAllPatient()) //전체 환자
+  const [waitings, setWaiting] = useState(waitingPatient()) //대기 환자
+  const [progresss, setProgress] = useState(progressPatient()) //진행 환자
+  const [completes, setComplete] = useState(completePatient()) //완료 환자
+
+  const [clickdateList, setClickDateList] = useState([]); //환자의 검사접수기록
+  const [clickdate, setClickdate] = useState({}) //클릭한 날짜의 검사접수 상세정보
+  const [profile, setProfile] = useState({}); //클릭한 환자의 프로필
  
-  const ClickPatient = (e, index, item) => {
+  const [groupshow, setGroupShow] = useState(false) //처방 보여주는 show
+ 
+  console.log(testreceptions)
+  const ClickPatient = (e, item) => {
     const reset = false;
     setGroupShow(reset)
-    const lists = [];
+    const visitLists = [];
     for(var i=0; i<testreceptions.length; i++){
-      const newpatientid = testreceptions[i];
-      if(newpatientid.patientid === item.patientid){
-        lists.push(newpatientid);  
+      if(testreceptions[i].patientid === item.patientid){
+        visitLists.push(testreceptions[i]); //검사받은 날의 객체 배열 저장
       }
     } 
-    setClickDate(lists)
+    setClickDateList(visitLists)
     setProfile(item)
   }
   
-  const ClickDate = (e, index, item) => {  
-    setPassData(item);
+  const onClickDate = (e, date) => {  
+    setClickdate(date); //클릭한 날짜 저장
+
     const value = true;
-    setGroupShow(value)
+    setGroupShow(value) //클릭시 show
   }
   
   return (
@@ -49,22 +48,22 @@ function TestPage(props) {
           <div style={{height:"88vh"}}>
           <PeriodSearch/>
           <Tab.Container id="left-tabs-example" defaultActiveKey="wait">
-            <Nav fill variant="tabs" className="flex-column mb-2">
-              <Row className="ml-0 mr-0">
-                <Nav.Item>
-                  <Nav.Link eventKey="total">전체<diV>{patient.length}</diV></Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="wait">대기<diV>{waiting.length}</diV></Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="progress">진행중<diV>{progress.length}</diV></Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="complete">완료<diV>{complete.length}</diV></Nav.Link>
-                </Nav.Item>
-              </Row>
-            </Nav>
+          <Nav fill variant="tabs" className="flex-column mb-2">
+            <Row className="ml-0 mr-0">
+              <Nav.Item>
+                <Nav.Link eventKey="total">전체<diV>{patients.length}</diV></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="wait">대기<diV>{waitings.length}</diV></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="progress">진행중<diV>{progresss.length}</diV></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="complete">완료<diV>{completes.length}</diV></Nav.Link>
+              </Nav.Item>
+            </Row>
+          </Nav>
           
           <div className="pt-2 pb-2 mb-2 d-flex align-items-center" style={{ backgroundColor:"#ffffff", boxShadow:"rgb(0 0 0 / 8%) 0px 0px 5px 2px", borderRadius:"15px", fontSize:"13px"}}>
             <div className="col-2 p-0 pt-1 pb-1 text-center">차트번호</div>
@@ -76,52 +75,51 @@ function TestPage(props) {
 
           <Tab.Content className="overflow-auto">
               <Tab.Pane eventKey= "total" className="pt-1">
-              {patient.map((item,index)=>{return(
-                <div className="pt-2 pb-2 mb-2 d-flex align-items-center" onClick={ e => {ClickPatient(e, index, item) }} style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
-                <div className="col-2 p-0 pt-1 pb-1 text-center">{patient[index].patientid}</div>
-                <div className="col-3 p-0 text-center">{patient[index].ssn1}</div>
-                <div className="col-1 p-0 text-center">{patient[index].sex}</div>
-                <div className="col-3 p-0 text-center">{patient[index].patientname}</div>
-                <div className="col-3 p-0 text-center"><Badge className="mr-1" variant="success">{patient[index].state}</Badge><Badge variant="danger">미입력</Badge></div>
+              {patients.map((item)=>{return(
+                <div className="pt-2 pb-2 mb-2 d-flex align-items-center" onClick={ e => {ClickPatient(e, item) }} style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
+                <div className="col-2 p-0 pt-1 pb-1 text-center">{item.patientid}</div>
+                <div className="col-3 p-0 text-center">{item.ssn1}</div>
+                <div className="col-1 p-0 text-center">{item.sex}</div>
+                <div className="col-3 p-0 text-center">{item.patientname}</div>
+                <div className="col-3 p-0 text-center"><Badge className="mr-1" variant="success">{item.state}</Badge><Badge variant="danger">미입력</Badge></div>
               </div>
               )})}
               </Tab.Pane>
               <Tab.Pane eventKey= "wait" className="pt-1">
-              {waiting.map((item,index)=>{return(
-                <div className="pt-2 pb-2 mb-2 d-flex align-items-center" onClick={ e => {ClickPatient(e, index, item) }} style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
-                <div className="col-2 p-0 pt-1 pb-1 text-center">{waiting[index].patientid}</div>
-                <div className="col-3 p-0 text-center">{waiting[index].ssn1}</div>
-                <div className="col-1 p-0 text-center">{waiting[index].sex}</div>
-                <div className="col-3 p-0 text-center">{waiting[index].patientname}</div>
-                <div className="col-3 p-0 text-center"><Badge className="mr-1" variant="success">{waiting[index].state}</Badge><Badge variant="danger">미입력</Badge></div>
+              {waitings.map((item)=>{return(
+                <div className="pt-2 pb-2 mb-2 d-flex align-items-center" onClick={ e => {ClickPatient(e, item) }} style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
+                <div className="col-2 p-0 pt-1 pb-1 text-center">{item.patientid}</div>
+                <div className="col-3 p-0 text-center">{item.ssn1}</div>
+                <div className="col-1 p-0 text-center">{item.sex}</div>
+                <div className="col-3 p-0 text-center">{item.patientname}</div>
+                <div className="col-3 p-0 text-center"><Badge className="mr-1" variant="success">{item.state}</Badge><Badge variant="danger">미입력</Badge></div>
               </div>
               )})}
               </Tab.Pane>
               <Tab.Pane eventKey= "progress" className="pt-1">
-              {progress.map((item,index)=>{return(
-                <div className="pt-2 pb-2 mb-2 d-flex align-items-center" onClick={ e => {ClickPatient(e, index, item) }} style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
-                <div className="col-2 p-0 pt-1 pb-1 text-center">{progress[index].patientid}</div>
-                <div className="col-3 p-0 text-center">{progress[index].ssn1}</div>
-                <div className="col-1 p-0 text-center">{progress[index].sex}</div>
-                <div className="col-3 p-0 text-center">{progress[index].patientname}</div>
-                <div className="col-3 p-0 text-center"><Badge className="mr-1" variant="success">{progress[index].state}</Badge><Badge variant="danger">미입력</Badge></div>
+              {progresss.map((item)=>{return(
+                <div className="pt-2 pb-2 mb-2 d-flex align-items-center" onClick={ e => {ClickPatient(e, item) }} style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
+                <div className="col-2 p-0 pt-1 pb-1 text-center">{item.patientid}</div>
+                <div className="col-3 p-0 text-center">{item.ssn1}</div>
+                <div className="col-1 p-0 text-center">{item.sex}</div>
+                <div className="col-3 p-0 text-center">{item.patientname}</div>
+                <div className="col-3 p-0 text-center"><Badge className="mr-1" variant="success">{item.state}</Badge><Badge variant="danger">미입력</Badge></div>
               </div>
               )})}
               </Tab.Pane>
               <Tab.Pane eventKey= "complete" className="pt-1">
-              {complete.map((item,index)=>{return(
-                <div className="pt-2 pb-2 mb-2 d-flex align-items-center" onClick={ e => {ClickPatient(e, index, item) }} style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
-                <div className="col-2 p-0 pt-1 pb-1 text-center">{complete[index].patientid}</div>
-                <div className="col-3 p-0 text-center">{complete[index].ssn1}</div>
-                <div className="col-1 p-0 text-center">{complete[index].sex}</div>
-                <div className="col-3 p-0 text-center">{complete[index].patientname}</div>
-                <div className="col-3 p-0 text-center"><Badge className="mr-1" variant="success">{complete[index].state}</Badge><Badge variant="danger">미입력</Badge></div>
+              {completes.map((item)=>{return(
+                <div className="pt-2 pb-2 mb-2 d-flex align-items-center" onClick={ e => {ClickPatient(e, item) }} style={{ fontSize:"13px", borderBottom:"1px solid #a6a6a6"}}>
+                <div className="col-2 p-0 pt-1 pb-1 text-center">{item.patientid}</div>
+                <div className="col-3 p-0 text-center">{item.ssn1}</div>
+                <div className="col-1 p-0 text-center">{item.sex}</div>
+                <div className="col-3 p-0 text-center">{item.patientname}</div>
+                <div className="col-3 p-0 text-center"><Badge className="mr-1" variant="success">{item.state}</Badge><Badge variant="danger">미입력</Badge></div>
               </div>
               )})}
               </Tab.Pane>
             </Tab.Content>
           </Tab.Container>
-
           </div>
         </div>
         <div className="col-5 pt-3">
@@ -140,14 +138,14 @@ function TestPage(props) {
               <div style={{width:"18%", marginRight:"3%", marginLeft:"2%"}}>
                 <div className="mb-3">검사 날짜: </div>
                 <div className="overflow-auto">   
-                  {clickdate.map((item,index)=>{return(   
-                  <div key={item.testreceptionid} className="pt-2 pb-2 mb-2 align-items-center" onClick={ e => { ClickDate(e, index, item) }} style={{border:"1px solid #dadada", borderRadius:"15px", textAlign:"center", backgroundColor:"#ffffff"}}>
-                    <div>{clickdate[index].testdate}</div>
+                  {clickdateList.map((date,index)=>{return(   
+                  <div key={date.testreceptionid} className="pt-2 pb-2 mb-2 align-items-center" onClick={ e => { onClickDate(e, date) }} style={{border:"1px solid #dadada", borderRadius:"15px", textAlign:"center", backgroundColor:"#ffffff"}}>
+                    <div>{date.testdate}</div>
                   </div>
                   )})}
                 </div>
               </div>
-              <div style={{width:"75%", marginRight:"2%"}}><div>검사 목록: </div>{groupshow?<TestGroup passdata={passdata}/>:""}</div>
+              <div style={{width:"75%", marginRight:"2%"}}><div>검사 목록: </div>{groupshow?<TestGroup clickdate={clickdate}/>:""}</div>
             </div>
         </div>
         <div className="col-4 pt-3" style={{borderLeft:"1px solid #dadada"}}>
