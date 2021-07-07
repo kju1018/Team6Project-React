14dde0c16fd1878fb610f1834508b54f98c71b76
jjung-222 <lyj7877@naver.com>
2021-07-07 18:39:19 +0900

    test

--- src/apis/test.js
+++ src/apis/test.js
@@ -5,36 +5,13 @@ export function testlistByDate(startdate, enddate) {
   const promise = axios.get(BASEURL+"test/patientbytestdate", {params:{startdate, enddate}})
   return promise
 }
-export function testlistByPatientid() {
-  const promise = axios.get(BASEURL+"test/testreceptionbypatientid")
+export function testlistByPatientid(patientid) {
+  console.log(patientid)
+  const promise = axios.get(BASEURL+"test/testreceptionbypatientid", {params: {patientid}})
   return promise
 }
 
 export function testlistByReceptionid(testreceptionid) {
-  const promise = axios.get(BASEURL+"test/testlistbyreceptionid/" + testreceptionid)
-  console.log(promise.data);
+  const promise = axios.get(BASEURL+"test/testlistbyreceptionid", {params: {testreceptionid}})
+  return promise
 }
-
-
-// export function testReceptions() {
-//   const [testreception, setTestrecception] = useState([]);
-
-//   useEffect(()=>{
-//     const promise = axios.get(BASEURL+"/test/patientbytestdate")
-//     console.log("ddd" + promise)
-//   })
- 
-// }
-
-// export function waitingPatient() {
-//   const patients = data.filter(patient => patient.state === "대기중");
-//   return patients;
-// }
-// export function progressPatient() {
-//   const patients = data.filter(patient => patient.state === "진행중");
-//   return patients;
-// }
-// export function completePatient() {
-//   const patients = data.filter(patient => patient.state === "완료");
-//   return patients;
-// }
