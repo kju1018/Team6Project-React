89b1eab1664ee68cad87081163fac7230bc2d5e2
KIM M S <61860897+kju1018@users.noreply.github.com>
2021-07-15 13:52:03 +0900

    수정

--- src/views/Treatment/DignosisList.js
+++ src/views/Treatment/DignosisList.js
@@ -13,22 +13,45 @@ function DiagnosisList(props) {
   const info = () => {
     alert("대기중인 진료를 선택해주세요.");
   }
-
   const prescribeDiagnoses =  (prescriptionItems) => {
     props.prescribeDiagnoses(prescriptionItems);
   }
 
   return (
     <>
-    {console.log(props.treatmentDiagnoses)}
       <ButtonHeader headertitle="상병 목록" iclassName="bi bi-check2-square" color="#D27E7B" btnicon="bi bi-plus-square" onclick={props.treatment.status==="진료 대기"? handleShow : info}/>
       <PrescriptionDignosesModal show={show} treatment={props.treatment} handleClose={handleClose} staticItemList={props.staticDignoses} itemList={props.treatmentDiagnoses} prescribe={prescribeDiagnoses}></PrescriptionDignosesModal>
       <div className="overflow-auto p-3" style={{height:"calc(100% - 50px"}}>
-      {props.treatmentDiagnoses.map (diagnosis => {
-          return (
-            <DignosesItem key={diagnosis.diagnosesdataid} item={diagnosis}></DignosesItem>
-          );
-      })}
+
+      
+      {
+        props.treatment.treatmentid == null ? 
+        <div className="h-100 d-flex align-items-center justify-content-center">
+          진료를 선택해주세요.
+        </div>
+        :
+        (
+          props.loading === true ? 
+          <div class="d-flex h-100 justify-content-center align-items-center">
+            <div class="spinner-border text-success" role="status">
+              <span class="sr-only">Loading...</span>
+            </div>
+          </div>
+          :
+          (
+            props.treatmentDiagnoses.length !==0 ? 
+            props.treatmentDiagnoses.map (diagnosis => {
+              return (
+                <DignosesItem key={diagnosis.diagnosesdataid} item={diagnosis}></DignosesItem>
+              );
+            })
+            :
+            <div className="h-100 d-flex align-items-center justify-content-center">
+              처방 받은 내역이 없습니다.
+            </div>
+          )
+        )
+      }
       </div>
 
       
