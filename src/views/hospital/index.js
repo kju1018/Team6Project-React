f119f73bb3460f6886d50113b3e14b494c7df6d7
kju1018 <kms951018@naver.com>
2021-07-22 17:48:08 +0900

    수정

--- src/views/hospital/index.js
+++ src/views/hospital/index.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import {  Redirect, Route, Switch, useHistory } from "react-router-dom";
+import {  Link, Redirect, Route, Switch, useHistory } from "react-router-dom";
 import { createSetAuthTokenAction, createSetCodeNumberAction, createSetRoleAuthority, createSetUseridAction } from "redux/auth-rducer";
 import Treatment from "views/Treatment";
 import HospitalNotice from "./HospitalNotice";
@@ -45,13 +45,11 @@ function Hospital(props) {
       [event.target.name]:event.target.value
     })
   }
-
   //만약 로그인이 되어 있다면 홈으로 이동
   if(globalUserid != null && globalUserid !== "") {
-    return <Redirect to="/main"></Redirect>
+    return <Redirect to="/main/home"></Redirect>
     // history.push('/main');
   } 
-
 
   const handleLogin = (event) => {
 
@@ -116,37 +114,37 @@ function Hospital(props) {
             <div className="card" style={{width:"50%", height:"30%", marginLeft:"25%", marginTop:"12%"}}>
               <div className="card-header" style={{backgroundColor:"rgb(18, 60, 114)", color:"white", fontWeight:"bold", fontSize:"24px"}}>LOGIN</div>
                 <div className="card-body row mt-4 ml-2" style={{height:"410px"}}>
-                <div className="col-5" style={{height:"390px"}}>
-                <Form noValidate validated={validated} onSubmit={handleLogin}>
+                  <div className="col-5" style={{height:"390px"}}>
+                  <Form noValidate validated={validated} onSubmit={handleLogin}>
 
-                    <Form.Group controlId="formBasicID">
-                      <Form.Label><div style={{fontWeight:"bold"}}>Hospital Code</div></Form.Label>
-                      <Form.Control required type="text" name="codenumber" placeholder="병원코드(숫자)를 입력하세요." value={loginForm.codenumber} onChange={handleChange} isInvalid={isInvalidCode}/>
-                      <Form.Control.Feedback type="invalid">{errorMessageCode}</Form.Control.Feedback>
-                    </Form.Group>
+                      <Form.Group controlId="formBasicID">
+                        <Form.Label><div style={{fontWeight:"bold"}}>Hospital Code</div></Form.Label>
+                        <Form.Control required type="text" name="codenumber" placeholder="병원코드(숫자)를 입력하세요." value={loginForm.codenumber} onChange={handleChange} isInvalid={isInvalidCode}/>
+                        <Form.Control.Feedback type="invalid">{errorMessageCode}</Form.Control.Feedback>
+                      </Form.Group>
 
-                    <Form.Group controlId="formBasicID">
-                      <Form.Label><div style={{fontWeight:"bold"}}>UserID</div></Form.Label>
-                      <Form.Control required type="text" name="userid" placeholder="직원 ID를 입력하세요." value={loginForm.userid} onChange={handleChange} isInvalid={isInvalidUserid}/>
-                      <Form.Control.Feedback type="invalid">{errorMessageUserid}</Form.Control.Feedback>
-                    </Form.Group>
+                      <Form.Group controlId="formBasicID">
+                        <Form.Label><div style={{fontWeight:"bold"}}>UserID</div></Form.Label>
+                        <Form.Control required type="text" name="userid" placeholder="직원 ID를 입력하세요." value={loginForm.userid} onChange={handleChange} isInvalid={isInvalidUserid}/>
+                        <Form.Control.Feedback type="invalid">{errorMessageUserid}</Form.Control.Feedback>
+                      </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                      <Form.Label><div style={{fontWeight:"bold"}}>Password</div></Form.Label>
-                      <Form.Control required type="password" autoComplete="off" name="userpassword" placeholder="비밀번호를 입력하세요." value={loginForm.userpassword} onChange={handleChange} isInvalid={isInvalidPassword}/>
-                      <Form.Control.Feedback type="invalid">{errorMessagePassword}</Form.Control.Feedback>
-                    </Form.Group>
-                    <div className="text-center mt-4">
-                      <Button variant="dark" block type="submit">
-                        로그인
-                      </Button>
-                    </div>
-                  </Form>
-                </div>
-                <div className="ml-4 mr-4" style={{borderLeft:"1px solid #dddddd", height:"340px"}}></div>
-                <div className="col-6">
-                  <HospitalNotice></HospitalNotice>
-                </div>
+                      <Form.Group controlId="formBasicPassword">
+                        <Form.Label><div style={{fontWeight:"bold"}}>Password</div></Form.Label>
+                        <Form.Control required type="password" autoComplete="off" name="userpassword" placeholder="비밀번호를 입력하세요." value={loginForm.userpassword} onChange={handleChange} isInvalid={isInvalidPassword}/>
+                        <Form.Control.Feedback type="invalid">{errorMessagePassword}</Form.Control.Feedback>
+                      </Form.Group>
+                      <div className="text-center mt-4">
+                        <Button variant="dark" block type="submit">
+                          로그인
+                        </Button>
+                      </div>
+                    </Form>
+                  </div>
+                  <div className="ml-4 mr-4" style={{borderLeft:"1px solid #dddddd", height:"340px"}}></div>
+                  <div className="col-6">
+                    <HospitalNotice></HospitalNotice>
+                  </div>
 
                 </div>
             </div>
